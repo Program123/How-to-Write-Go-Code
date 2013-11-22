6faf80993f26b6104b1e7a735cf5f7b16d9c49da
Matthias Luebken <matthias.luebken@gmail.com>
2013-11-22 10:46:08 +0100

    Initial commit.

---
+++ src/hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/user/newmath"
+	"hello/helper"
+)
+
+func main() {
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", newmath.Sqrt(2))
+	helper.Help()
+}
---
+++ src/hello/helper/helper.go
@@ -0,0 +1,9 @@
+package helper
+
+import (
+	"fmt"
+)
+
+func Help() {
+	fmt.Printf("Glad I can help out.\n")
+}
